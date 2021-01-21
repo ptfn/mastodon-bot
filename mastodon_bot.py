69a8cd77de1644dd71a5a03634c0c986d4f9541e
ptfn <CipherDogs@protonmail.com>
2021-01-21 22:03:32 +0700

    Added file

---
+++ mastodon_bot.py
@@ -0,0 +1,39 @@
+import requests
+import schedule
+import os
+import time
+
+token = os.getenv('TOKEN')
+coins = ['btc', 'eth', 'xmr', 'ltc', 'etc', 'dot', 'grin']
+url = "https://botsin.space/api/v1/statuses"
+
+
+def price_coin(arr):
+    r = requests.get('https://www.bw.com/exchange/config/controller/website/pricecontroller/getassistprice')
+    data = r.json()
+    string = ''
+    for i in range(len(arr)):
+        price = data['datas']['usd'][arr[i]]
+        string_coin = arr[i]
+        string = string + string_coin.upper() + ': ' + price + '$' + '\n'
+    return string
+
+price_coin = price_coin(coins)
+headers = {"Authorization": "Bearer " + token}
+body = {"status": price_coin }
+
+
+def run_func():
+    r = requests.post(url, headers = headers, json = body, timeout = 10)
+
+
+schedule.every().day.at("10:00").do(print_coin)
+schedule.every().day.at("13:00").do(print_coin)
+schedule.every().day.at("16:00").do(print_coin)
+schedule.every().day.at("19:00").do(print_coin)
+schedule.every().day.at("22:00").do(print_coin)
+
+
+while True:
+    schedule.run_pending()
+    time.sleep(1)
