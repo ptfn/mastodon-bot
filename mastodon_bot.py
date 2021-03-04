dc0ec59ef977ef95c93fdbc022fcc71b4e9d8d03
ptfn <CipherDogs@protonmail.com>
2021-03-04 21:32:08 +0700

    added persent

--- mastodon_bot.py
+++ mastodon_bot.py
@@ -3,22 +3,28 @@ import schedule
 import os
 import time
 
-token = os.getenv('TOKEN')
-coins = ['btc', 'eth', 'xmr', 'ltc', 'etc', 'dot','grin', 'ksm']
+token = "X6rsLusoReALFfsrKbXg96CoDjHyZ18Wkh_4Qzj8pFE"#os.getenv('TOKEN')
+coins = ['btc', 'eth', 'xmr', 'dot', 'ksm']
 url = "https://botsin.space/api/v1/statuses"
+last_price = {'btc' : 0, 'eth' : 0, 'xmr' : 0, 'dot' : 0, 'ksm' : 0}
 
+
+def persent_price(last, new):
+    return round((new - last) / last * 100, 2)
 
 def price_coin(arr):
     string = ''
     for i in range(len(arr)):
-        try:
-            r = requests.get('https://api.bitfinex.com/v1/pubticker/'+ arr[i] +'usd')
-            data = r.json()
-            price = data['ask']
-            string_coin = arr[i]
-            string = string + string_coin.upper() + ': ' + price + '$' + '\n'
-        except:
-            print('Error')
+        r = requests.get('https://api.bitfinex.com/v1/pubticker/'+ arr[i] +'usd')
+        data = r.json()
+        price = data['ask']
+        coin = arr[i]
+        if last_price[coin] == 0:
+            string = string + coin.upper() + ': ' + price + '$' + '\n'
+        else:
+            per = persent_price(float(last_price[coin]), float(price))
+            string = string + coin.upper() + ': ' + '$' + price + ' ({}%)'.format(per) + '\n'
+        last_price[coin] = price
     return string
 
 
@@ -28,7 +34,7 @@ def run_func(price_coin):
     r = requests.post(url, headers = headers, json = body, timeout = 10)
 
 
-schedule.every().hour.do(run_func, price_coin)
+schedule.every(5).seconds.do(run_func, price_coin)
 
 
 while True:
