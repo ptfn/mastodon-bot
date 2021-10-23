a5b9a879b814489f10643c3a51e401db91c00bea
ptfn <CipherDogs@protonmail.com>
2021-10-23 13:25:04 +0700

    added coins

--- mastodon_bot.py
+++ mastodon_bot.py
@@ -5,8 +5,8 @@ import time
 
 token = "jPropEfy21SbhvPSVMzOFQe7SoE18KYWQ4rzfEb6c9Y"#os.getenv('TOKEN')
 url = "https://botsin.space/api/v1/statuses"
-coins = ['btc', 'eth', 'ltc', 'xmr', 'uni', 'dot', 'ksm']
-last_price = {'btc' : 0, 'eth' : 0, 'ltc' : 0, 'xmr' : 0, 'uni' : 0, 'dot' : 0, 'ksm' : 0}
+coins = ['btc', 'eth', 'ltc', 'xmr', 'uni', 'dot', 'ksm', 'ada', 'xrp']
+last_price = {'btc' : 0, 'eth' : 0, 'ltc' : 0, 'xmr' : 0, 'uni' : 0, 'dot' : 0, 'ksm' : 0, 'ada' : 0, 'xrp' : 0}
 
 
 def persent_price(last, new):
