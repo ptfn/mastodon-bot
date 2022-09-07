59886431608417d91ea71f8f8d67d9fad7d77c89
ptfn <CipherDogs@protonmail.com>
2022-09-08 00:02:44 +0700

    fixed

--- mastodon_bot.py
+++ mastodon_bot.py
@@ -6,8 +6,12 @@ import schedule
 
 token = os.getenv('TOKEN')
 url = "https://botsin.space/api/v1/statuses"
-coins = ['btc', 'eth', 'ltc', 'xmr', 'uni', 'dot', 'ksm', 'ada', 'xrp']
-last_price = {'btc' : 0, 'eth' : 0, 'ltc' : 0, 'xmr' : 0, 'uni' : 0, 'dot' : 0, 'ksm' : 0, 'ada' : 0, 'xrp' : 0}
+coins = ['btc', 'eth', 'ltc',
+         'xmr', 'uni', 'dot',
+         'ksm', 'ada', 'xrp']
+last_price = {'btc': 0, 'eth': 0, 'ltc': 0,
+              'xmr': 0, 'uni': 0, 'dot': 0,
+              'ksm': 0, 'ada': 0, 'xrp': 0}
 
 
 def persent_price(last, new):
@@ -16,28 +20,24 @@ def persent_price(last, new):
 
 def price_coin(arr):
     string = ''
-    
     for i in range(len(arr)):
-        r = requests.get('https://api.bitfinex.com/v1/pubticker/'+ arr[i] +'usd')
+        r = requests.get('https://api.bitfinex.com/v1/pubticker/'+arr[i]+'usd')
         data = r.json()
         price = data['ask']
         coin = arr[i]
-        
         if last_price[coin] == 0:
             string = string + coin.upper() + ': ' + '$' + price + '\n'
-        
         else:
             per = persent_price(float(last_price[coin]), float(price))
-            string = string + coin.upper() + ': ' + '$' + price + ' ({}%)'.format(per) + '\n'
+            string += f"{coin.upper()}: $ {price} {per}%\n"
         last_price[coin] = price
-    
     return string
 
 
 def request(price_coin):
     headers = {"Authorization": "Bearer " + token}
-    body = {"status": price_coin(coins) }
-    r = requests.post(url, headers = headers, json = body, timeout = 60)
+    body = {"status": price_coin(coins)}
+    r = requests.post(url, headers=headers, json=body, timeout=60)
 
 
 def main():
@@ -46,13 +46,10 @@ def main():
     while True:
         try:
             schedule.run_pending()
-        
-        except:
+        except Exception:
             print("--Request Error!--")
-        
         else:
             print("--Request Ok!--")
-        
         finally:
             time.sleep(15)
 
